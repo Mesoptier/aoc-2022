0e7fb2979a36cdb81fc5b0755a827704e4b81b97
Koen Klaren <koen.klaren@gmail.com>
2022-12-02 19:54:09 +0100

    Refactor part 1 to use BinaryHeap

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,9 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use itertools::Itertools;
 
-fn both_parts(input: &str) -> Option<(u32, u32)> {
-    let mut sums = input
+fn solve(input: &str, max_len: usize) -> Option<u32> {
+    input
         .lines()
         // Map to Option<u32>
         .map(|line| {
@@ -20,33 +22,28 @@ fn both_parts(input: &str) -> Option<(u32, u32)> {
                 }
                 Some(sum)
             }
-        });
-
-    let max_len = 3;
-
-    let mut maximum_sums = sums.by_ref().take(max_len).collect_vec();
-    maximum_sums.sort_unstable_by(|a, b| b.cmp(a));
-
-    for sum in sums {
-        if sum > maximum_sums[max_len - 1] {
-            maximum_sums.pop();
-            maximum_sums.push(sum);
-            maximum_sums.sort_unstable_by(|a, b| b.cmp(a));
-        }
-    }
-
-    maximum_sums
-        .first()
-        .cloned()
-        .zip(maximum_sums.into_iter().sum1())
+        })
+        // Record the `max_len` largest sums
+        .fold(BinaryHeap::<Reverse<u32>>::new(), |mut min_heap, sum| {
+            if min_heap.len() < max_len {
+                min_heap.push(Reverse(sum));
+            } else if min_heap.peek().unwrap().0 < sum {
+                min_heap.pop();
+                min_heap.push(Reverse(sum));
+            }
+            min_heap
+        })
+        .into_iter()
+        .map(|rev| rev.0)
+        .sum1()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    both_parts(input).map(|result| result.0)
+    solve(input, 1)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    both_parts(input).map(|result| result.1)
+    solve(input, 3)
 }
 
 fn main() {

8fd3bb0d05bd2bbda50078a00ea51b0f10f82098
Koen Klaren <koen.klaren@gmail.com>
2022-12-06 21:38:54 +0100

    Simplify and fastify some more

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,39 +1,38 @@
-use std::collections::VecDeque;
-
 fn solve(input: &str, marker_len: usize) -> Option<usize> {
     let input = input.as_bytes();
     let (input_head, input_tail) = input.split_at(marker_len);
 
-    let mut buffer = VecDeque::from_iter(input_head);
+    // Map from character index ('a'=0, 'b'=1, ..., 'z'=25) to the number of
+    // occurences in the last `marker_len` characters.
     let mut counts = [0; 26];
-    let mut duplicates_in_buffer = 0;
+    // Number of duplicates in the last `marker_len` characters.
+    let mut duplicates = 0;
 
     for c in input_head {
         let idx = (*c as usize) - ('a' as usize);
         counts[idx] += 1;
         if counts[idx] >= 2 {
-            duplicates_in_buffer += 1;
+            duplicates += 1;
         }
     }
 
-    for (position, new_char) in input_tail.into_iter().enumerate() {
-        if duplicates_in_buffer == 0 {
+    for (position, &new_char) in input_tail.into_iter().enumerate() {
+        if duplicates == 0 {
             return Some(position + marker_len);
         }
 
-        let old_char = buffer.pop_front().unwrap();
-        buffer.push_back(new_char);
+        let old_char = input[position];
 
-        let old_char_idx = (*old_char as usize) - ('a' as usize);
+        let old_char_idx = (old_char as usize) - ('a' as usize);
         counts[old_char_idx] -= 1;
         if counts[old_char_idx] >= 1 {
-            duplicates_in_buffer -= 1;
+            duplicates -= 1;
         }
 
-        let new_char_idx = (*new_char as usize) - ('a' as usize);
+        let new_char_idx = (new_char as usize) - ('a' as usize);
         counts[new_char_idx] += 1;
         if counts[new_char_idx] >= 2 {
-            duplicates_in_buffer += 1;
+            duplicates += 1;
         }
     }
 

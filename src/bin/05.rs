1ba7e1b34b049abc32566b7ae3da126a63823e91
Koen Klaren <koen.klaren@gmail.com>
2022-12-10 10:51:52 +0100

    Move get_pair_mut to helpers

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,4 @@
+use advent_of_code::helpers::get_pair_mut;
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -47,21 +48,6 @@ fn parse_moves(input: &str) -> IResult<&str, Vec<(u32, u32, u32)>> {
             preceded(tag(" to "), map_res(digit1, str::parse::<u32>)),
         )),
     )(input)
-}
-
-/// Returns mutable references to a pair of indices at once.
-///
-/// Panics if `index1 == index2` or either index is out of range.
-fn get_pair_mut<T>(slice: &mut [T], (index1, index2): (usize, usize)) -> (&mut T, &mut T) {
-    assert_ne!(index1, index2, "indices must not be the same");
-
-    if index1 < index2 {
-        let (left, right) = slice.split_at_mut(index2);
-        (&mut left[index1], &mut right[0])
-    } else {
-        let (left, right) = slice.split_at_mut(index1);
-        (&mut right[0], &mut left[index2])
-    }
 }
 
 pub fn part_one(input: &str) -> Option<String> {
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,14 @@
-/*
- * Use this file if you want to extract helpers from your solutions.
- * Example import from this file: `use advent_of_code::helpers::example_fn;`.
- */
+/// Returns mutable references to a pair of indices at once.
+///
+/// Panics if `index1 == index2` or either index is out of range.
+pub fn get_pair_mut<T>(slice: &mut [T], (index1, index2): (usize, usize)) -> (&mut T, &mut T) {
+    assert_ne!(index1, index2, "indices must not be the same");
+
+    if index1 < index2 {
+        let (left, right) = slice.split_at_mut(index2);
+        (&mut left[index1], &mut right[0])
+    } else {
+        let (left, right) = slice.split_at_mut(index1);
+        (&mut right[0], &mut left[index2])
+    }
+}

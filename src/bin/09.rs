17bfeaa8861dc29c2fe321a0a79ff6b6239dc7a3
Koen Klaren <koen.klaren@gmail.com>
2022-12-10 10:53:49 +0100

    Day 9, part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 
+use advent_of_code::helpers::get_pair_mut;
 use nom::{
     bytes::complete::tag,
     character::complete::{anychar, digit1},
@@ -12,40 +13,46 @@ fn parse_line(input: &str) -> IResult<&str, (char, u32)> {
     separated_pair(anychar, tag(" "), map_res(digit1, str::parse::<u32>))(input)
 }
 
-pub fn part_one(input: &str) -> Option<usize> {
+fn solve(input: &str, rope_len: usize) -> Option<usize> {
     let motions = input.lines().map(|line| parse_line(line).unwrap().1);
 
     let mut visited = HashSet::<(i32, i32)>::new();
-
-    let mut h_pos = (0i32, 0i32);
-    let mut t_pos = (0i32, 0i32);
+    let mut rope = vec![(0i32, 0i32); rope_len];
 
     for (dir, steps) in motions {
         for _step in 0..steps {
             match dir {
-                'L' => h_pos.0 -= 1,
-                'R' => h_pos.0 += 1,
-                'U' => h_pos.1 += 1,
-                'D' => h_pos.1 -= 1,
+                'L' => rope[0].0 -= 1,
+                'R' => rope[0].0 += 1,
+                'U' => rope[0].1 += 1,
+                'D' => rope[0].1 -= 1,
                 _ => unreachable!(),
             }
 
-            let diff = ((h_pos.0 - t_pos.0), (h_pos.1 - t_pos.1));
-            if diff.0.abs() >= 2 || (h_pos.1 - t_pos.1).abs() >= 2 {
-                let normalized_diff = (diff.0.signum(), diff.1.signum());
-                t_pos.0 += normalized_diff.0;
-                t_pos.1 += normalized_diff.1;
+            for i in 1..rope_len {
+                let (h_pos, t_pos) = get_pair_mut(&mut rope, (i - 1, i));
+
+                let diff = ((h_pos.0 - t_pos.0), (h_pos.1 - t_pos.1));
+                if diff.0.abs() >= 2 || (h_pos.1 - t_pos.1).abs() >= 2 {
+                    let normalized_diff = (diff.0.signum(), diff.1.signum());
+                    t_pos.0 += normalized_diff.0;
+                    t_pos.1 += normalized_diff.1;
+                }
             }
 
-            visited.insert(t_pos);
+            visited.insert(rope[rope_len - 1]);
         }
     }
 
     Some(visited.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<usize> {
+    solve(input, 2)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    solve(input, 10)
 }
 
 fn main() {
@@ -67,6 +74,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1));
     }
 }

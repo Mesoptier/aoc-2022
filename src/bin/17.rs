62ea97948f848beb2c750116c7dbf53990733d6b
Koen Klaren <koen.klaren@gmail.com>
2022-12-25 16:02:14 +0100

    Day 17, part 2

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,3 +1,5 @@
+use hashbrown::{hash_map::Entry, HashMap};
+
 enum Push {
     Left,
     Right,
@@ -10,33 +12,6 @@ const ROCK_SHAPES: [[u8; 4]; 5] = [
     [0b01000000, 0b01000000, 0b01000000, 0b01000000],
     [0b01100000, 0b01100000, 0b00000000, 0b00000000],
 ];
-
-fn print_rocks(chamber: &Vec<u8>, rock: &[u8; 4], rock_y: usize) {
-    let height = chamber.len() + 7;
-
-    for y in (0..height).rev() {
-        let chamber_row = *chamber.get(y).unwrap_or(&0);
-        let rock_row = if rock_y <= y && y < rock_y + 4 {
-            rock[y - rock_y]
-        } else {
-            0
-        };
-
-        print!("|");
-        for x in (0..7).rev() {
-            if rock_row & (1 << x) != 0 {
-                print!("@");
-            } else if chamber_row & (1 << x) != 0 {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!("|");
-    }
-    println!("+-------+");
-    println!();
-}
 
 pub fn part_one(input: &str) -> Option<usize> {
     let mut jet_pattern = input
@@ -59,8 +34,6 @@ pub fn part_one(input: &str) -> Option<usize> {
         }
 
         let mut y = chamber.len() + 3;
-
-        //        print_rocks(&chamber, &rock, y);
 
         loop {
             match jet_pattern.next().unwrap() {
@@ -129,8 +102,128 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(chamber.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let jet_pattern = input
+        .chars()
+        .filter_map(|c| match c {
+            '<' => Some(Push::Left),
+            '>' => Some(Push::Right),
+            _ => None,
+        })
+        .collect::<Vec<_>>();
+    let jet_pattern_len = jet_pattern.len();
+    let mut jet_pattern = jet_pattern.iter().cycle();
+
+    let mut rock_shapes = ROCK_SHAPES.iter().cycle();
+
+    let mut chamber: Vec<u8> = vec![];
+
+    let max_steps: usize = 1000000000000;
+    let min_cycle_len = jet_pattern_len * 5;
+
+    const SIGNATURE_LEN: usize = 20;
+    let mut signatures = HashMap::<[u8; SIGNATURE_LEN], (usize, usize)>::new();
+
+    let mut skipped_height = None;
+    let mut step = 0;
+    while step < max_steps {
+        if step != 0 && step % min_cycle_len == 0 && skipped_height.is_none() {
+            let height = chamber.len();
+            let signature: [u8; SIGNATURE_LEN] = chamber[(chamber.len() - SIGNATURE_LEN)..]
+                .try_into()
+                .unwrap();
+            match signatures.entry(signature) {
+                Entry::Occupied(entry) => {
+                    let (prev_step, prev_height) = *entry.get();
+                    let diff_step = step - prev_step;
+                    let diff_height = height - prev_height;
+
+                    let remaining_steps = max_steps - step;
+                    let skip_cycles = remaining_steps / diff_step;
+
+                    step += skip_cycles * diff_step;
+                    skipped_height = Some(skip_cycles * diff_height);
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert((step, height));
+                }
+            }
+        }
+
+        let mut rock = *rock_shapes.next().unwrap();
+        for row in &mut rock {
+            *row = *row >> 2;
+        }
+
+        let mut y = chamber.len() + 3;
+
+        loop {
+            match jet_pattern.next().unwrap() {
+                Push::Left => {
+                    let can_move_left = rock.iter().enumerate().all(|(dy, row)| {
+                        if (row & (1 << 6)) != 0 {
+                            false
+                        } else if y + dy < chamber.len() {
+                            chamber[y + dy] & (row << 1) == 0
+                        } else {
+                            true
+                        }
+                    });
+                    if can_move_left {
+                        for row in &mut rock {
+                            *row = *row << 1;
+                        }
+                    }
+                }
+                Push::Right => {
+                    let can_move_right = rock.iter().enumerate().all(|(dy, row)| {
+                        if (row & 1) != 0 {
+                            false
+                        } else if y + dy < chamber.len() {
+                            chamber[y + dy] & (row >> 1) == 0
+                        } else {
+                            true
+                        }
+                    });
+                    if can_move_right {
+                        for row in &mut rock {
+                            *row = *row >> 1;
+                        }
+                    }
+                }
+            }
+
+            let can_move_down = y > 0
+                && rock.iter().enumerate().all(|(dy, row)| {
+                    if y + dy - 1 < chamber.len() {
+                        chamber[y + dy - 1] & row == 0
+                    } else {
+                        true
+                    }
+                });
+            if can_move_down {
+                y -= 1;
+            } else {
+                for (dy, &row) in rock.iter().enumerate() {
+                    if row == 0 {
+                        continue;
+                    }
+
+                    if y + dy >= chamber.len() {
+                        chamber.resize(y + dy + 1, 0);
+                    }
+
+                    chamber[y + dy] |= row;
+                }
+
+                break;
+            }
+        }
+
+        step += 1;
+    }
+
+    Some(chamber.len() + skipped_height.unwrap())
 }
 
 fn main() {
@@ -152,6 +245,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 17);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1514285714288));
     }
 }

bc1b0ef024b3728a6945198e356e46cf27f0b729
Koen Klaren <koen.klaren@gmail.com>
2022-12-26 22:00:37 +0100

    Day 24, part 2

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -43,9 +43,9 @@ type Coord = (usize, usize);
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, PartialOrd, Ord)]
 enum Position {
-    Goal,
+    TopLeft,
     Valley(Coord),
-    Start,
+    BottomRight,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -77,12 +77,6 @@ struct State {
     pos: Position,
 }
 
-impl State {
-    fn is_goal(&self) -> bool {
-        self.pos == Position::Goal
-    }
-}
-
 #[derive(PartialEq, Eq)]
 struct HeapEntry(usize, State);
 
@@ -98,7 +92,7 @@ impl PartialOrd for HeapEntry {
     }
 }
 
-pub fn part_one(input: &str) -> Option<usize> {
+fn parse_blizzard_grid(input: &str) -> BlizzardGrid {
     let blizzards: Vec<Vec<Option<Direction>>> = input
         .lines()
         .skip(1)
@@ -123,29 +117,34 @@ pub fn part_one(input: &str) -> Option<usize> {
         })
         .collect();
 
-    let grid = BlizzardGrid {
+    BlizzardGrid {
         width: blizzards[0].len(),
         height: blizzards.len(),
         blizzards,
-    };
+    }
+}
 
-    let grid_width = grid.width;
-    let grid_height = grid.height;
+fn shortest_path(grid: &BlizzardGrid, start_state: State, goal_pos: Position) -> Option<usize> {
+    let max_x = grid.width - 1;
+    let max_y = grid.height - 1;
 
     let get_valid_neighbors = |pos: Position| match pos {
-        Position::Start => vec![Position::Valley((0, 0)), Position::Start],
-        Position::Goal => unreachable!(),
+        Position::TopLeft => vec![Position::Valley((0, 0)), Position::TopLeft],
+        Position::BottomRight => vec![Position::Valley((max_y, max_x)), Position::BottomRight],
         Position::Valley((y, x)) => {
             let mut neighbors = vec![];
 
-            if y + 1 == grid_height && x + 1 == grid_width {
-                neighbors.push(Position::Goal);
+            if y == max_y && x == max_x {
+                neighbors.push(Position::BottomRight);
+            }
+            if y == 0 && x == 0 {
+                neighbors.push(Position::TopLeft)
             }
 
-            if x + 1 < grid_width {
+            if x < max_x {
                 neighbors.push(Position::Valley((y, x + 1)));
             }
-            if y + 1 < grid_height {
+            if y < max_y {
                 neighbors.push(Position::Valley((y + 1, x)));
             }
             if x > 0 {
@@ -161,31 +160,34 @@ pub fn part_one(input: &str) -> Option<usize> {
         }
     };
 
-    let goal_dist = |pos: Position| match pos {
-        Position::Goal => 0,
-        Position::Valley((y, x)) => (grid_height - y) + (grid_width - x) - 1,
-        Position::Start => grid_width + grid_height,
-    };
+    let goal_dist = |pos: Position| {
+        let dist = match pos {
+            Position::BottomRight => 0,
+            Position::Valley((y, x)) => (max_y - y) + (max_x - x) + 1,
+            Position::TopLeft => max_y + max_x + 2,
+        };
 
-    let start_state = State {
-        time: 0,
-        pos: Position::Start,
+        match goal_pos {
+            Position::TopLeft => max_y + max_x + 2 - dist,
+            Position::BottomRight => dist,
+            Position::Valley(_) => unreachable!(),
+        }
     };
 
     let mut visited = HashSet::<State>::new();
     let mut heap = BinaryHeap::<HeapEntry>::new();
 
     visited.insert(start_state);
-    heap.push(HeapEntry(goal_dist(Position::Start), start_state));
+    heap.push(HeapEntry(goal_dist(goal_pos), start_state));
 
-    while let Some(HeapEntry(cost, state)) = heap.pop() {
-        if state.is_goal() {
+    while let Some(HeapEntry(_, state)) = heap.pop() {
+        if state.pos == goal_pos {
             return Some(state.time);
         }
 
         for pos in get_valid_neighbors(state.pos) {
             let is_reachable = match pos {
-                Position::Start | Position::Goal => true,
+                Position::TopLeft | Position::BottomRight => true,
                 Position::Valley(coord) => grid.is_cell_empty(coord, state.time + 1),
             };
             if !is_reachable {
@@ -193,7 +195,7 @@ pub fn part_one(input: &str) -> Option<usize> {
             }
 
             let new_state = State {
-                time: (state.time + 1) % (grid_width * grid_height),
+                time: (state.time + 1),
                 pos,
             };
 
@@ -212,8 +214,46 @@ pub fn part_one(input: &str) -> Option<usize> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<usize> {
+    let grid = parse_blizzard_grid(input);
+    shortest_path(
+        &grid,
+        State {
+            time: 0,
+            pos: Position::TopLeft,
+        },
+        Position::BottomRight,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let grid = parse_blizzard_grid(input);
+    let time = shortest_path(
+        &grid,
+        State {
+            time: 0,
+            pos: Position::TopLeft,
+        },
+        Position::BottomRight,
+    )
+    .unwrap();
+    let time = shortest_path(
+        &grid,
+        State {
+            time,
+            pos: Position::BottomRight,
+        },
+        Position::TopLeft,
+    )
+    .unwrap();
+    shortest_path(
+        &grid,
+        State {
+            time,
+            pos: Position::TopLeft,
+        },
+        Position::BottomRight,
+    )
 }
 
 fn main() {
@@ -235,6 +275,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 24);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(54));
     }
 }

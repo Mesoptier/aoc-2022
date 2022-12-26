fe2bb6c0f18edef4e3f689191bffaee761456b7e
Koen Klaren <koen.klaren@gmail.com>
2022-12-26 10:47:22 +0100

    Day 20, part 2

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,14 +1,14 @@
 #[derive(Debug)]
 struct Node {
-    value: i32,
+    value: i64,
     prev_idx: usize,
     next_idx: usize,
 }
 
-pub fn part_one(input: &str) -> Option<i32> {
+pub fn solve(input: &str, decryption_key: i64, mix_count: usize) -> Option<i64> {
     let file = input
         .lines()
-        .map(|line| line.parse::<i32>().unwrap())
+        .map(|line| line.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
 
     let len = file.len();
@@ -16,41 +16,43 @@ pub fn part_one(input: &str) -> Option<i32> {
         .into_iter()
         .enumerate()
         .map(|(idx, value)| Node {
-            value,
+            value: value * decryption_key,
             prev_idx: (idx + len - 1) % len,
             next_idx: (idx + 1) % len,
         })
         .collect::<Vec<_>>();
 
-    for idx in 0..len {
-        let Node {
-            value,
-            prev_idx,
-            next_idx,
-        } = nodes[idx];
+    for _ in 0..mix_count {
+        for idx in 0..len {
+            let Node {
+                value,
+                prev_idx,
+                next_idx,
+            } = nodes[idx];
 
-        // Remove node from list
-        nodes[prev_idx].next_idx = next_idx;
-        nodes[next_idx].prev_idx = prev_idx;
+            // Remove node from list
+            nodes[prev_idx].next_idx = next_idx;
+            nodes[next_idx].prev_idx = prev_idx;
 
-        // Find new position
-        let mut prev_idx = prev_idx;
-        let mut next_idx = next_idx;
-        for _ in 0..value.abs() {
-            if value > 0 {
-                prev_idx = next_idx;
-                next_idx = nodes[next_idx].next_idx;
-            } else {
-                next_idx = prev_idx;
-                prev_idx = nodes[prev_idx].prev_idx;
-            };
+            // Find new position
+            let mut prev_idx = prev_idx;
+            let mut next_idx = next_idx;
+            for _ in 0..(value.abs() as usize % (len - 1)) {
+                if value > 0 {
+                    prev_idx = next_idx;
+                    next_idx = nodes[next_idx].next_idx;
+                } else {
+                    next_idx = prev_idx;
+                    prev_idx = nodes[prev_idx].prev_idx;
+                };
+            }
+
+            // Insert node into new position
+            nodes[prev_idx].next_idx = idx;
+            nodes[next_idx].prev_idx = idx;
+            nodes[idx].prev_idx = prev_idx;
+            nodes[idx].next_idx = next_idx;
         }
-
-        // Insert node into new position
-        nodes[prev_idx].next_idx = idx;
-        nodes[next_idx].prev_idx = idx;
-        nodes[idx].prev_idx = prev_idx;
-        nodes[idx].next_idx = next_idx;
     }
 
     let zero_idx = nodes.iter().position(|node| node.value == 0).unwrap();
@@ -68,8 +70,12 @@ pub fn part_one(input: &str) -> Option<i32> {
     Some(mixed_file[1000 % len] + mixed_file[2000 % len] + mixed_file[3000 % len])
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i64> {
+    solve(input, 1, 1)
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
+    solve(input, 811589153, 10)
 }
 
 fn main() {
@@ -91,6 +97,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 20);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1623178306));
     }
 }

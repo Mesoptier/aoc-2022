468199e5798a72678e9b3f9633f0a6b89f9b4fcd
Koen Klaren <koen.klaren@gmail.com>
2022-12-26 18:56:51 +0100

    Fix day 21 test

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -235,6 +235,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 21);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(301));
     }
 }
